4820591c214d5a4d81e0cfb80d5174f467b40f98
Shreyash Gaurav <shreyash2856@gmail.com>
2025-07-26 17:37:38 +0530

    Added streamlit db <- FreeSQLDatabase

--- config.py
+++ config.py
@@ -1,19 +1,29 @@
 import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
-
-# API Configuration
-OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
-OPENWEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
-
-# Database Configuration
-DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'localhost'),
-    'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_NAME', 'weather_analytics')
-}
-
-# Cities to track
+try:
+    
+    DB_CONFIG = {
+        'host': st.secrets["database"]["DB_HOST"],
+        'user': st.secrets["database"]["DB_USER"],
+        'password': st.secrets["database"]["DB_PASSWORD"],
+        'database': st.secrets["database"]["DB_NAME"],
+        'port': int(st.secrets["database"]["DB_PORT"])
+    }
+    OPENWEATHER_API_KEY = st.secrets["api"]["OPENWEATHER_API_KEY"]
+    OPENWEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+    
+except:
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    DB_CONFIG = {
+        'host': os.getenv('DB_HOST', 'sql12.freesqldatabase.com'),
+        'user': os.getenv('DB_USER', 'sql12792014'),
+        'password': os.getenv('DB_PASSWORD', 'your-actual-password'),
+        'database': os.getenv('DB_NAME', 'sql12792014'),
+        'port': int(os.getenv('DB_PORT', 3306))
+    }
+    OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+    OPENWEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 CITIES = ['London', 'New York', 'Tokyo', 'Mumbai', 'Sydney']
